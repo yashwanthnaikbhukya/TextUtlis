d6b6b2694344d028f023f727491a5d0cc4abc89f
yashwanthnaikbhukya <yashwanthnaikbhukya18@gmail.com>
2024-05-26 18:07:28 +0530

    added replace feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,6 +21,37 @@ export default function TextForm(props) {
         props.showAlert("Cleared", "success")
     }
 
+    const handleSentence = () =>{
+        let words = text.split(" ");
+        let sentence = "";
+        for (let i = 0; i < words.length; i++){
+            words[i] = 
+            sentence += words[i].slice(0,1).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
+        }
+        setText(sentence);
+        props.showAlert("Converted to Sentence case", "success")
+    }
+
+    const handleCopy = () =>{
+        navigator.clipboard.writeText(text);
+    }
+
+    const handleReplace =() =>{
+        var x = document.querySelector('#textFind');
+        var y = document.querySelector('#textReplace');
+        if (x.value === null || x.value.trim() === "") {
+            props.showAlert("Find field is empty, Please enter the word to find and replace", "warning");
+        }
+        else if(text.includes(x.value) === false){
+            props.showAlert("No match found", "danger");
+        }
+        else{
+            let newText = text.replaceAll(x.value, y.value);
+            setText(newText);
+            props.showAlert("Replaced", "success");
+        }
+    }
+
   const [text, setText] = useState("Enter the text here .......");
   return (
     <>
@@ -31,7 +62,24 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleSentence}>Sentence case</button>
             <button className="btn btn-primary mx-2" onClick={handleClear}>Clear</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy} >Copy</button>
+        </div>
+        <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
+            <div class="container my-3">
+                <div class="row align-items-center">
+                    <div class="col-auto">
+                    <label htmlFor="textFind">Find</label>
+                    <input class="form-control" type="text" id="textFind" style={{backgroundColor: props.mode === 'dark'?'#161031':'white', color: props.mode === 'dark'?'white':'black'}} />
+                    </div>
+                    <div class="col-auto">
+                    <label htmlFor="textReplace">Replace</label>
+                    <input class="form-control" type="text" id="textReplace" style={{backgroundColor: props.mode === 'dark'?'#161031':'white', color: props.mode === 'dark'?'white':'black'}} />
+                    </div>
+                </div>
+                <button className="btn btn-primary my-3" onClick={handleReplace} >Replace</button>
+            </div>
         </div>
         <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
             <h1>Text summary</h1>
